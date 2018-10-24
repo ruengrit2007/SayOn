bcae3c9da04bec4cb95efc990fffdd0f732d89b2
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-25 03:03:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,104 +1,36 @@
 <?php
-  function pubMqtt($topic,$msg){
-    $APPID= "SayOn/"; //enter your appid
-    $KEY = "ALVaMBoxFRm7MrO"; //enter your key
-    $SECRET = "EYaq59mVqT5N0Py8XUhj8ASRh"; //enter your secret
-    $Topic = "$topic"; 
-      put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
- 
-  }
- function getMqttfromlineMsg($Topic,$lineMsg){
- 
-    $pos = strpos($lineMsg, ":");
-    if($pos){
-      $splitMsg = explode(":", $lineMsg);
-      $topic = $splitMsg[0];
-      $msg = $splitMsg[1];
-      pubMqtt($topic,$msg);
-    }else{
-      $topic = $Topic;
-      $msg = $lineMsg;
-      pubMqtt($topic,$msg);
-    }
-  }
- 
-  function put($url,$tmsg)
-{
-      
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);    
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
- 
-    //curl_setopt($ch, CURLOPT_USERPWD, "ALVaMBoxFRm7MrO:EYaq59mVqT5N0Py8XUhj8ASRh");
-     
-    $response = curl_exec($ch);
-    
-      curl_close($ch);
-      echo $response . "\r\n";
-    return $response;
-}
- //$Topic = "NodeMCU1";
- //$lineMsg = "CHECK";
- //getMqttfromlineMsg($Topic,$lineMsg);
- function send_LINE($msg){
- $access_token = 'pUSNzzZen370jm5bI5kLCmfbikfsjNmZ91BDlseT7uqm4psB6dUFRa9iC8grOXVCzHahbJoufFH/ez6U0j0ZcwHjX9tiXs3k4fECDbuQNOArtHhAl0BOm4GLen8gM12h8+YzOdqNAmdGMy+UTtxn9gdB04t89/1O/w1cDnyilFU='; 
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/push';
-      $data = [
-        'to' => 'U71b27d2ec856cb314af237d9fc0061bc',
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   $accessToken = "";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   $content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   //รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+   //รับ id ของผู้ใช้
+   $id = $arrayJson['events'][0]['source']['userId'];
+   #ตัวอย่าง Message Type "Text + Sticker"
+   if($message == "สวัสดี"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+   }
+   function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       $result = curl_exec($ch);
-      curl_close($ch);
-      echo $result . "\r\n"; 
-}
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['ESP'])) {
-	
-	send_LINE($events['ESP']);
-		
-	echo "OK";
-	}
-if (!is_null($events['events'])) {
-	echo "line bot";
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$Topic = "NodeMCU1" ;
-			getMqttfromlineMsg($Topic,$text);
-			   
-			
-		}
-	}
-}
-$Topic = "NodeMCU1" ;
-$text = "Test123";
-getMqttfromlineMsg($Topic,$text);
-echo "OK3";
+      curl_close ($ch);
+   }
+   exit;
 ?>
