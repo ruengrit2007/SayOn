5da4ce2151de4e347932a0d665f6a1fd0023bb51
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-25 01:27:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,15 +26,10 @@
 {
       
     $ch = curl_init($url);
- 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-     
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-     
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-     
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-     
     curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
  
     //curl_setopt($ch, CURLOPT_USERPWD, "ALVaMBoxFRm7MrO:EYaq59mVqT5N0Py8XUhj8ASRh");
