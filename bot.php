65cc73fb02b22764a9183e6cb82af870a1533cd6
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-25 15:03:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -52,24 +52,24 @@ $events = json_decode($content, true);
 		
 	//echo "OK";
 	//}
-if (!is_null($events['events'])) {
-	echo "line bot";
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$Topic = "NodeMCU1" ;
-			getMqttfromlineMsg($Topic,$text);
-			   
-			
-		}
-	}
-}
+//if (!is_null($events['events'])) {
+//	echo "line bot";
+////	// Loop through each event
+//	foreach ($events['events'] as $event) {
+//		// Reply only when message sent is in 'text' format
+//		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+//			// Get text sent
+//			$text = $event['message']['text'];
+//			// Get replyToken
+//			$replyToken = $event['replyToken'];
+//			// Build message to reply back
+//			$Topic = "NodeMCU1" ;
+//			getMqttfromlineMsg($Topic,$text);
+//			   
+//			
+//		}
+//	}
+//}
 $Topic = "NodeMCU1" ;
 $text = "Test123459";
 getMqttfromlineMsg($Topic,$text);
