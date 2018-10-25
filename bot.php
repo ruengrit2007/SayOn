848c60df99989e7fb3b836ccca4e95175a65e488
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-25 14:56:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -70,12 +70,12 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
+//if (!is_null($events['ESP'])) {
 	
-	send_LINE($events['ESP']);
+	//send_LINE($events['ESP']);
 		
-	echo "OK";
-	}
+	//echo "OK";
+	//}
 if (!is_null($events['events'])) {
 	echo "line bot";
 	// Loop through each event
