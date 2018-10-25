0856c152f5919d12adf5d35f9730ca514a43eae4
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-25 13:06:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,28 +1,46 @@
-<?php
-  
-function send_LINE($msg){
- $access_token = 'pUSNzzZen370jm5bI5kLCmfbikfsjNmZ91BDlseT7uqm4psB6dUFRa9iC8grOXVCzHahbJoufFH/ez6U0j0ZcwHjX9tiXs3k4fECDbuQNOArtHhAl0BOm4GLen8gM12h8+YzOdqNAmdGMy+UTtxn9gdB04t89/1O/w1cDnyilFU=';
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/push';
-      $data = [
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
+ <?php
+ function pubMqtt($topic,$msg){
+       $APPID= "numpapicklinebot/"; //enter your appid
+     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
+    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+    $Topic = "$topic"; 
+      put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
+ 
+  }
+ function getMqttfromlineMsg($Topic,$lineMsg){
+ 
+    $pos = strpos($lineMsg, ":");
+    if($pos){
+      $splitMsg = explode(":", $lineMsg);
+      $topic = $splitMsg[0];
+      $msg = $splitMsg[1];
+      pubMqtt($topic,$msg);
+    }else{
+      $topic = $Topic;
+      $msg = $lineMsg;
+      pubMqtt($topic,$msg);
+    }
+  }
+ 
+  function put($url,$tmsg)
+{
+      
+    $ch = curl_init($url); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+ 
+    //curl_setopt($ch, CURLOPT_USERPWD, "mJ7K4MfteC7p0dW:pp4gzMhCvJIqlxc66hKEvk46m");
+     
+    $response = curl_exec($ch);
+    
       curl_close($ch);
-      echo $result . "\r\n"; 
+      echo $response . "\r\n";
+    return $response;
 }
+// $Topic = "NodeMCU1";
+ //$lineMsg = "CHECK";
+ //getMqttfromlineMsg($Topic,$lineMsg);
 ?>
